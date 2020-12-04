b6df3386501aa7d3a83bde9ef5ee96836dfae5bd
Coder Girl <52979210+codergirl-al@users.noreply.github.com>
2020-12-04 01:52:45 +0100

    Added react-hook-form and implemented form-validation

--- books-client/src/components/AddBook.js
+++ books-client/src/components/AddBook.js
@@ -1,126 +1,204 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import BookDataService from "../services/BookService";
 
 const AddBook = () => {
-    const initialBookState = {
-        id: null,
-        title: "",
-        author: "",
-        description: "",
-        rating: "",
-        published: false
-    };
-    const [book, setBook] = useState(initialBookState);
-    const [submitted, setSubmitted] = useState(false);
+  const { register, handleSubmit, errors, formState } = useForm();
+  const initialBookState = {
+    id: null,
+    title: "",
+    author: "",
+    rating: "",
+    pages: "",
+    language: "",
+    description: "",
+    publisher_id: "",
+    published: false,
+  };
+  const [book, setBook] = useState(initialBookState);
+  const [submitted, setSubmitted] = useState(false);
 
-    const handleInputChange = event => {
-        const { name, value } = event.target;
-        setBook({ ...book, [name]: value });
-    };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setBook({ ...book, [name]: value });
+  };
 
-    const saveBook = () => {
-        var data = {
-            title: book.title,
-            description: book.description,
-            author: book.author,
-            rating: book.rating,
-            published: book.published,
-        };
-
-        BookDataService.create(data)
-            .then(response => {
-                setBook({
-                    id: response.data.id,
-                    title: response.data.title,
-                    author: response.data.author,
-                    rating: response.data.rating,
-                    description: response.data.description,
-                    published: response.data.published
-                });
-                setSubmitted(true);
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
+  const saveBook = () => {
+    var data = {
+      title: book.title,
+      author: book.author,
+      rating: book.rating,
+      pages: book.pages,
+      language: book.language,
+      description: book.description,
+      publisher_id: "",
+      published: book.published,
     };
 
-    const newBook = () => {
-        setBook(initialBookState);
-        setSubmitted(false);
-    };
-    return (
-        <div className="submit-form">
-            {submitted ? (
-                <div>
-                    <h4>You submitted successfully!</h4>
-                    <button className="btn btn-success" onClick={newBook}>
-                        Add
-              </button>
-                </div>
-            ) : (
-                    <div>
-                        <h1 className="text-center text-info">ADD BOOK</h1>
-                        <hr />
-                        <div className="form-group">
-                            <label htmlFor="title">Title</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="title"
-                                required
-                                value={book.title}
-                                onChange={handleInputChange}
-                                name="title"
-                            />
-                        </div>
+    BookDataService.create(data)
+      .then((response) => {
+        setBook({
+          id: response.data.id,
+          title: response.data.title,
+          author: response.data.author,
+          rating: response.data.rating,
+          pages: response.data.pages,
+          language: response.data.language,
+          description: response.data.description,
+          publisher_id: response.data.publisher_id,
+          published: response.data.published,
+        });
+        setSubmitted(true);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const onSubmit = () => {
+    if (formState.errors.data) return;
+    saveBook();
+  };
 
-                        <div className="form-group">
-                            <label htmlFor="author">Author</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="title"
-                                required
-                                value={book.author}
-                                onChange={handleInputChange}
-                                name="author"
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="rating">Rating</label>
-                            <input
-                                type="number"
-                                className="form-control"
-                                id="title"
-                                required
-                                value={book.rating}
-                                onChange={handleInputChange}
-                                name="rating"
-                            />
-                        </div>
-
-
-                        <div className="form-group">
-                            <label htmlFor="description">Description</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="description"
-                                required
-                                value={book.description}
-                                onChange={handleInputChange}
-                                name="description"
-                            />
-                        </div>
-
-                        <button onClick={saveBook} className="btn btn-primary">
-                            Submit
-              </button>
-                    </div>
-                )}
+  const newBook = () => {
+    setBook(initialBookState);
+    setSubmitted(false);
+  };
+  return (
+    <form className="submit-form" onSubmit={handleSubmit(onSubmit)}>
+      {submitted ? (
+        <div>
+          <h4>You submitted successfully!</h4>
+          <button className="btn btn-success" onClick={newBook}>
+            Add
+          </button>
         </div>
-    );
+      ) : (
+        <div>
+          <h1 className="text-center text-info">ADD BOOK</h1>
+          <hr />
+          <div className="form-group">
+            <label htmlFor="title">
+              Title<span className="text-danger">*</span>
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              ref={register({ required: true })}
+              id="title"
+              value={book.title}
+              onChange={handleInputChange}
+              name="title"
+            />
+            <span className="text-danger">
+              {errors.title && "Title is required"}
+            </span>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="author">
+              Author<span className="text-danger">*</span>
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="auth"
+              value={book.author}
+              ref={register({ required: true })}
+              onChange={handleInputChange}
+              name="author"
+            />
+            <span className="text-danger">
+              {errors.author && "Author is required"}
+            </span>
+          </div>
+          <div className="form-group">
+            <label htmlFor="rating">Rating</label>
+            <input
+              type="number"
+              className="form-control"
+              ref={register({ min: 0, max: 5 })}
+              id="title"
+              value={book.rating}
+              onChange={handleInputChange}
+              name="rating"
+            />
+            <span className="text-danger">
+              {errors.rating &&
+                "Invalid number for rating. Add a number between 0 and 5."}
+            </span>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="pages">
+              Pages<span className="text-danger">*</span>
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="pages"
+              ref={register({ min: 0, required: true })}
+              value={book.pages}
+              onChange={handleInputChange}
+              name="pages"
+            />
+            <span className="text-danger">
+              {errors.pages?.type == "required" && "Pages is required"}
+              {errors.pages?.type == "min" &&
+                "Please add a positive number for pages."}
+            </span>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="language">Language</label>
+            <input
+              type="text"
+              className="form-control"
+              id="language"
+              value={book.language}
+              onChange={handleInputChange}
+              name="language"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              type="text"
+              className="form-control"
+              placeholder="Add a short description for this book."
+              id="description"
+              value={book.description}
+              onChange={handleInputChange}
+              name="description"
+            />
+          </div>
+          <div htmlFor="publisher.id">
+            <select className="form-control">
+              <option>Please choose a publisher</option>
+              {publishers &&
+                publishers.map((publisher, index) => (
+                  <option
+                    className={
+                      "list-group-item " +
+                      (index === currentIndex ? "active" : "")
+                    }
+                    onClick={() => setActivePublisher(publisher, index)}
+                    key={index}
+                  >
+                    {publisher.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <br></br>
+          <br></br>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      )}
+    </form>
+  );
 };
 export default AddBook;
--- books-client/src/components/Book.js
+++ books-client/src/components/Book.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import BookDataService from "../services/BookService";
 
-const Book = props => {
+const Book = (props) => {
+  const { register, handleSubmit, errors, formState } = useForm();
   const initialBookState = {
     id: null,
     title: "",
     author: "",
     rating: "",
+    pages: "",
+    language: "",
     description: "",
-    published: false
+    publisher_id: "",
+    published: false,
   };
   const [currentBook, setCurrentBook] = useState(initialBookState);
   const [message, setMessage] = useState("");
 
-  const getBook = id => {
+  const getBook = (id) => {
     BookDataService.get(id)
-      .then(response => {
+      .then((response) => {
         setCurrentBook(response.data);
         console.log(response.data);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   };
@@ -28,51 +33,59 @@ const Book = props => {
     getBook(props.match.params.id);
   }, [props.match.params.id]);
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setCurrentBook({ ...currentBook, [name]: value });
   };
 
-  const updatePublished = status => {
+  const updatePublished = (status) => {
     var data = {
       id: currentBook.id,
       title: currentBook.title,
       author: currentBook.author,
       rating: currentBook.rating,
+      pages: currentBook.pages,
+      language: currentBook.language,
       description: currentBook.description,
-      published: status
+      publisher_id: currentBook.publisher_id,
+      published: status,
     };
 
     BookDataService.update(currentBook.id, data)
-      .then(response => {
+      .then((response) => {
         setCurrentBook({ ...currentBook, published: status });
         console.log(response.data);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   };
 
   const updateBook = () => {
     BookDataService.update(currentBook.id, currentBook)
-      .then(response => {
+      .then((response) => {
         console.log(response.data);
         setMessage("The book was updated successfully!");
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   };
 
   const deleteBook = () => {
     BookDataService.remove(currentBook.id)
-      .then(response => {
+      .then((response) => {
         console.log(response.data);
         props.history.push("/books");
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
+  };
+
+  const onSubmit = () => {
+    if (formState.errors.data) return;
+    updateBook();
   };
 
   return (
@@ -80,37 +93,84 @@ const Book = props => {
       {currentBook ? (
         <div className="edit-form">
           <h4>Book</h4>
-          <form>
-          <div className="form-group">
-              <label htmlFor="title">Title</label>
+          <form className="submit-form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor="title">
+                Title<span className="text-danger">*</span>
+              </label>
               <input
                 type="text"
                 className="form-control"
+                ref={register({ required: true })}
                 id="title"
                 name="title"
                 value={currentBook.title}
                 onChange={handleInputChange}
               />
+              <span className="text-danger">
+                {errors.title && "Title is required"}
+              </span>
             </div>
             <div className="form-group">
-              <label htmlFor="author">Author</label>
+              <label htmlFor="author">
+                Author<span className="text-danger">*</span>
+              </label>
               <input
                 type="text"
                 className="form-control"
+                ref={register({ required: true })}
                 id="author"
                 name="author"
                 value={currentBook.author}
                 onChange={handleInputChange}
               />
+              <span className="text-danger">
+                {errors.author && "Author is required"}
+              </span>
             </div>
             <div className="form-group">
               <label htmlFor="rating">Rating</label>
               <input
-                type="text"
+                type="number"
                 className="form-control"
+                ref={register({ min: 0, max: 5 })}
                 id="rating"
                 name="rating"
                 value={currentBook.rating}
+                onChange={handleInputChange}
+              />
+              <span className="text-danger">
+                {errors.rating &&
+                  "Invalid number for rating. Add a number between 0 and 5."}
+              </span>
+            </div>
+            <div className="form-group">
+              <label htmlFor="pages">
+                Pages<span className="text-danger">*</span>
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                ref={register({ min: 0, required: true })}
+                id="pages"
+                name="pages"
+                value={currentBook.pages}
+                onChange={handleInputChange}
+              />
+              <span className="text-danger">
+                {errors.pages?.type == "required" && "Pages is required"}
+                {errors.pages?.type == "min" &&
+                  "Please add a positive number for pages."}
+              </span>
+            </div>
+            <div className="form-group">
+              <label htmlFor="language">language</label>
+              <input
+                type="text"
+                className="form-control"
+                id="language"
+                name="language"
+                value={currentBook.language}
                 onChange={handleInputChange}
               />
             </div>
@@ -125,43 +185,48 @@ const Book = props => {
                 onChange={handleInputChange}
               />
             </div>
-
+            <div className="form-group">
+              <label htmlFor="publisher_id">Publisher:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="publisher_id"
+                name="publisher_id"
+                value={currentBook.publisher_id}
+                onChange={handleInputChange}
+              />
+            </div>
             <div className="form-group">
               <label>
                 <strong>Status:</strong>
               </label>
               {currentBook.published ? "Published" : "Pending"}
             </div>
+            {currentBook.published ? (
+              <button
+                className="badge badge-primary mr-2"
+                onClick={() => updatePublished(false)}
+              >
+                UnPublish
+              </button>
+            ) : (
+              <button
+                className="badge badge-primary mr-2"
+                onClick={() => updatePublished(true)}
+              >
+                Publish
+              </button>
+            )}
+
+            <button className="badge badge-danger mr-2" onClick={deleteBook}>
+              Delete
+            </button>
+
+            <button type="submit" className="badge badge-success">
+              Update
+            </button>
+            <p>{message}</p>
           </form>
-
-          {currentBook.published ? (
-            <button
-              className="badge badge-primary mr-2"
-              onClick={() => updatePublished(false)}
-            >
-              UnPublish
-            </button>
-          ) : (
-            <button
-              className="badge badge-primary mr-2"
-              onClick={() => updatePublished(true)}
-            >
-              Publish
-            </button>
-          )}
-
-          <button className="badge badge-danger mr-2" onClick={deleteBook}>
-            Delete
-          </button>
-
-          <button
-            type="submit"
-            className="badge badge-success"
-            onClick={updateBook}
-          >
-            Update
-          </button>
-          <p>{message}</p>
         </div>
       ) : (
         <div>
